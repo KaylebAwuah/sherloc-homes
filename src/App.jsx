002c575517602b08428db27a7a0b416e1710e4ad
Kwasi-Awuah <stawuah18@gmail.com>
2023-10-30 16:28:55 +0000

    Added pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
