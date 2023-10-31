43041b5f5866396e42f279d5bcbdc4ac424c09da
Kwasi-Awuah <stawuah18@gmail.com>
2023-10-31 16:05:44 +0000

    Tested api Route

---
+++ api/controllers/user.controllers.js
@@ -0,0 +1,5 @@
+const test = ((req, res) => {
+    res.json({ "meassage": " bacck" })
+})
+
+module.exports = { test }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const userRouter = require('./routes/user.route.js')
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -11,6 +12,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express()
 
+app.use('/api/user', userRouter)
+
 app.listen(3000, () => {
     console.log(`Server for sherloc is runing on 3000!!`);
 })
+
