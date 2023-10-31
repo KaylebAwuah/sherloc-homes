1b5e6f3f6a211b8c741e9e598500df07473ae2c6
Kwasi-Awuah <stawuah18@gmail.com>
2023-10-31 16:57:00 +0000

    signUp  functionality created

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRouter = require('./routes/user.route.js')
+const authRouter = require('./routes/auth.route.js')
 dotenv.config()
+
+
+const app = express()
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("SherLoc database connected");
@@ -10,9 +14,12 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 
-const app = express()
 
+
+app.use(express.json())
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
+
 
 app.listen(3000, () => {
     console.log(`Server for sherloc is runing on 3000!!`);
