c3313701f541b3ab35c12cc2155cfe6e433c88e6
KaylebAwuah <calebadomawuah@gmail.com>
2023-11-16 16:22:55 +0000

    faulty signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+
 const User = require('../models/user.model.js')
 const errorHandler = require('../utils/error.js')
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -21,4 +23,29 @@ const signup = async (req, res, next) => {
 }
 
 
-module.exports = { signup }
+const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, 'User not found!'))
+  
+      const validPassword = bcryptjs.compareSync(password, validUser.password)
+      if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
+  
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+  
+      // Log the token to the console
+      console.log('JWT Token:', token);
+  
+      const { password: pass, ...rest } = validUser._doc;
+  
+      res
+        .cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+module.exports = { signin , signup}
