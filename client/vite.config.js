dcf03f9270936f65c5bf21df33aee7e8cbb9846f
Kwasi <stawuah18@gmail.com>
2023-12-04 16:21:05 +0000

    Fix : vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server :{
+//     proxy :{
+//       '/api':{ 
+//       target: 'http://localhost:3000',
+//     secure : false
+//   },
+// },
+//   },
+//   plugins: [react()],
+// })
+
 export default defineConfig({
-  server :{
-    proxy :{
-      '/api':{ 
-      target: 'http://localhost:3000',
-    secure : false
-  },
-},
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+    hmr: {
+      overlay: true, // Enable overlay for HMR (Hot Module Replacement) errors
+    },
   },
   plugins: [react()],
-})
+  esbuild: {
+    jsxInject: 'import React from "react";', // Add this line if you're using JSX without importing React in every file
+  },
+});
+
